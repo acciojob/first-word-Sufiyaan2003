a0d3d11969f4a8dc360de2627c656dc6e287ad2e
Sufiyaan Tamboli <sufiyaantamboli1983@gmail.com>
2025-07-08 19:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstWord(s) {
   // your code here
+  let word = ""; 
+
+  // Remove leading spaces
+  let i = 0;
+  while (i < s.length && s[i] === ' ') {
+    i++;
+  }
+
+  // Read characters until the first space or end of string
+  while (i < s.length && s[i] !== ' ') {
+    word += s[i];
+    i++;
+  }
+
+  return word;
 }
 
 // Do not change the code below
